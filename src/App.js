3a93764e454cd2bd0dfa4dba87fae73c967313c1
eezj35 <justineezj0@gmail.com>
2022-05-25 18:31:00 +0800

    Implement feature to communicate with backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import './App.css';
 import {useState} from 'react';
 import Upload from './components/Upload'
+import axios from 'axios'
 
 function App() {
+  const [file, setFile] = useState(null);
 
-  const [files, setFiles] = useState([{
-    name: "myFile.pdf"
-  }])
+  const handleFile = (e) => {
+    setFile(e.target.files[0]);
+  }
 
+  const handleUpload = (e) => {
+    let formData = new FormData();
+    formData.append('file', file);
+    axios.post('http://localhost:8080/uploadFile', formData, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+    }).then((res) => {console.log('success')})
+    .catch((err) => {console.log(err)});
+  }
 
-  
 
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>menu</li>
-          <li>settings</li>
-        </ul>
-      </nav>
-      <Upload files={files} setFiles={setFiles} />
+
+      <form>
+        <div>
+          <input type="file" name="file" 
+          onChange = {handleFile}/>
+
+        </div>
+        <button onClick={handleUpload}> Upload </button>
+      </form>
+
     </div>
   );
 }
